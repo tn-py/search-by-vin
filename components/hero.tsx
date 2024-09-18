0ae6600cd5f4cffcb668f1b274b592a2b997206b
Tirso00 <tirso.an08@gmail.com>
2024-09-18 00:18:02 -0400

    changes to front page

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,16 +4,17 @@ import Spline from "@splinetool/react-spline/next";
 
 export default function Hero() {
   return (
-    <div className="relative flex flex-col gap-16 items-center">
+    <div className="relative flex flex-col gap-16 items-center w-full">
       {/* Large non-selectable text overlay on Spline */}
-      <h1 className="absolute top-1/4 text-4xl md:text-6xl font-bold text-white z-10 select-none">
+      <h1 className="absolute top-[10%] md:top-[15%] text-4xl md:text-6xl font-bold text-white z-10 select-none">
         Search by VIN
       </h1>
 
-      {/* Spline Design */}
-      <main className="w-full h-[500px] md:h-[700px]">
+      {/* Responsive Spline container */}
+      <main className="w-full aspect-w-16 aspect-h-9 max-w-screen-xl">
         <Spline
           scene="https://prod.spline.design/eVEg5O3J24l44iEv/scene.splinecode"
+          className="w-full h-full"
         />
       </main>
 
